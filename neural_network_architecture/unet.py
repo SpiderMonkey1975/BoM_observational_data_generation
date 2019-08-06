243d2c78b185d5a5130a4d9b1a47ceb9a48a1abf
Mark Cheeseman <mark.cheeseman@pawsey.org.au>
2019-08-06 05:52:41 +0000

    working regression prototype with U-Net support

--- neural_network_architecture/unet.py
+++ neural_network_architecture/unet.py
@@ -1,11 +1,8 @@
 import tensorflow as tf
-import tensorflow.keras.backend as K
 
 from tensorflow.keras.models import Sequential, Model
-from tensorflow.keras.layers import Activation, Dense, Reshape, multiply, add, Dropout, AveragePooling2D, GlobalAveragePooling2D
-from tensorflow.keras.layers import Input, BatchNormalization, Conv2D, Conv2DTranspose, MaxPooling2D, concatenate, UpSampling2D
+from tensorflow.keras.layers import Dropout, Input, BatchNormalization, Conv2D, MaxPooling2D, concatenate, UpSampling2D
 from tensorflow.keras.utils import multi_gpu_model
-from tensorflow.keras.regularizers import l2
 
 
 def construct_model( input_layer, output_layer, num_gpu ):
@@ -37,25 +34,53 @@ def unet_decoder_block( net, conv, num_filters ):
     return net
 
 def unet( num_filters, num_gpus ):
-    ''' Python function that defines U-Net architecture of an autoencoder neural network
+    ''' Python function that defines a modified U-Net autoencoder neural net architecture
 
         INPUT: num_filters -> # of filters in the first convolutional layer
                num_gpus    -> # of GPUs used in training the network
     '''
-    input_layer = Input(shape = (2050, 2450, 1))
 
-    conv_list = []
+    input_layer = Input(shape = (2050, 2450, 1))
 
     net = Conv2D( 1, 3, strides=1, activation='relu', padding='same')(input_layer)
     net = Conv2D(num_filters, 3, strides=1, activation='relu', padding='same')(net)
-    conv_list.append( Conv2D(num_filters, 3, strides=1, activation='relu', padding='same')(net) )
+    conv1 = Conv2D(num_filters, 3, strides=1, activation='relu', padding='same')(net)
 
-    for n in range(1,5):
-        conv_list.append( unet_encoder_block( conv_list[n-1], num_filters*(2**n) ))
+    net = MaxPooling2D(2)( conv1 )
+    net = BatchNormalization(axis=3)( net )
+    net = Conv2D(num_filters*2, 3, strides=1, activation='relu', padding='same')(net)
+    conv2 = Conv2D(num_filters*2, 3, strides=1, activation='relu', padding='same')(net)
 
-    net = conv_list[4]
-    for n in range(3,-1,-1):
-        net = unet_decoder_block( net, conv_list[n], num_filters*(2**n) )
+    net = MaxPooling2D(5)( conv2 )
+    net = BatchNormalization(axis=3)( net )
+    net = Conv2D(num_filters*4, 3, strides=1, activation='relu', padding='same')(net)
+    conv3 = Conv2D(num_filters*4, 3, strides=1, activation='relu', padding='same')(net)
+
+    net = MaxPooling2D(5)( conv3 )
+    net = BatchNormalization(axis=3)( net )
+    net = Conv2D(num_filters*8, 3, strides=1, activation='relu', padding='same')(net)
+
+    net = UpSampling2D(5)(net)
+    net = concatenate( [net,conv3],axis=3 )
+    net = Conv2D(num_filters*4, 3, strides=1, activation='relu', padding='same')(net)
+    net = Conv2D(num_filters*4, 3, strides=1, activation='relu', padding='same')(net)
+    net = BatchNormalization(axis=3)( net )
+
+    net = UpSampling2D(5)(net)
+    net = concatenate( [net,conv2],axis=3 )
+    net = Conv2D(num_filters*2, 3, strides=1, activation='relu', padding='same')(net)
+    net = Conv2D(num_filters*2, 3, strides=1, activation='relu', padding='same')(net)
+    net = BatchNormalization(axis=3)( net )
+
+    net = UpSampling2D(2)(net)
+    net = concatenate( [net,conv1],axis=3 )
+    net = Conv2D(num_filters, 3, strides=1, activation='relu', padding='same')(net)
+    net = Conv2D(num_filters, 3, strides=1, activation='relu', padding='same')(net)
+    net = BatchNormalization(axis=3)( net )
+
+    #net = conv_list[4]
+    #for n in range(3,-1,-1):
+    #    net = unet_decoder_block( net, conv_list[n], num_filters*(2**n) )
 
     net = Conv2D(1, 3, strides=1, activation='relu', padding='same')(net)
 
